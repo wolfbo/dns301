06792417aecebf63b4859e47147843bb42df447a
Wolfgang Wangerin <w.wangerin@hawesko.de>
2016-09-02 11:10:05 +0200

    updated start.sh to allow setup for testing

--- start.sh
+++ start.sh
@@ -1,6 +1,21 @@
 #!/bin/bash
 
 clear
+echo "installing python-virtualenv"
+pip install virtualenv
+
+echo "initialising virtualenv"
+virtualenv virtualenv
+
+echo "activating virtualenv"
+source virtualenv/bin/activate
+
+echo "installing requirements into virtualenv"
+pip install -r requirements.txt
+
+export PORT=5000
 export FLASK_DEBUG=1
 export FLASK_APP=dns301.py
-python -m flask run
+
+echo "starting flaskapp on port $PORT"
+python -m flask run --port $PORT
